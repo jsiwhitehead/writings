b8ce9bb61b77a3d0e275cef99f4521239cbd6a81
Jon Whitehead <jsiwhitehead@gmail.com>
2020-04-04 22:03:05 +0100

    Updated downloads and removed pdfs

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -1,9 +1,7 @@
 import * as fs from 'fs-extra';
-import * as PDFParser from 'pdf2json';
 
 import { stringify } from '../util';
 
-import parsePdf from './pdf';
 import scrape from './scrape';
 import structure from './structure';
 
@@ -13,29 +11,15 @@ import structure from './structure';
   await Promise.all(
     files.map(async (f) => {
       const config = require(`../books/${f}`).config;
-      if (config.url.endsWith('.pdf')) {
-        await new Promise((resolve) => {
-          const pdfParser = new PDFParser();
-          pdfParser.on('pdfParser_dataReady', async (data) => {
-            await fs.writeFile(
-              `./data/parsed/${f}.json`,
-              stringify(parsePdf(data, config.start, config.end)),
-            );
-            resolve();
-          });
-          pdfParser.loadPDF(`./data/downloaded/${f}.pdf`);
-        });
-      } else {
-        const html = await fs.readFile(`./data/downloaded/${f}.html`, 'utf8');
-        const parsed = structure(
-          scrape(html, config.classes, config.titleJoin),
-          config.start,
-          config.end,
-          config.smallBreak,
-          config.titleJoin,
-        );
-        await fs.writeFile(`./data/parsed/${f}.json`, stringify(parsed));
-      }
+      const html = await fs.readFile(`./data/downloaded/${f}.html`, 'utf8');
+      const parsed = structure(
+        scrape(html, config.classes, config.titleJoin),
+        config.start,
+        config.end,
+        config.smallBreak,
+        config.titleJoin,
+      );
+      await fs.writeFile(`./data/parsed/${f}.json`, stringify(parsed));
     }),
   );
 
